480b752056631f0ef54425a1d2e48fa7b1126cbf
Pedro Navarrete Hurtado <navhpedro@gmail.com>
2018-04-12 01:18:47 -0500

    Exercise 1 Completed

--- COMP151LabChapter12/files.cpp
+++ COMP151LabChapter12/files.cpp
@@ -1,5 +1,6 @@
 
 
+
 // This program uses hours, pay rate, state tax and fed tax to determine gross
 // and net pay.
 
@@ -8,9 +9,14 @@
 #include <iomanip>
 using namespace std;
 
+// Pedro J. Navarrete
+// V0.00
+
 int main()
 {
 	// Fill in the code to define payfile as an input file
+	ifstream payfile;
+	ofstream outPayfile("pay.out", ios::out);
 	float gross;
 	float net;
 	float hours;
@@ -18,31 +24,29 @@ int main()
     float stateTax;
 	float fedTax;
 
-	cout << fixed << setprecision(2) << showpoint;
+	outPayfile << fixed << setprecision(2) << showpoint;
 	// FILL IN THE CODE TO OPEN payfile AND ATTACH IT TO THE PHYSICAL FILE
     // NAMED payroll.dat
+	payfile.open("payroll.txt");
 
 	// FILL IN CODE TO WRITE A CONDITIONAL STATEMENT TO CHECK IF payfile 
 	// DOES NOT EXIST.
+	if(!payfile)
 	{
-		cout << "Error opening file. \n";
-		cout << "It may not exist where indicated" << endl;
+		outPayfile << "Error opening file. \n";
+		outPayfile << "It may not exist where indicated" << endl;
 		return 1;
 	}
 
-	cout << "Payrate      Hours    Gross Pay       Net Pay" 
-           << endl   << endl;      
-     // FILL IN CODE TO PRIME THE READ FOR THE payfile FILE.
-     // FILL IN CODE TO WRITE A LOOP CONDITION TO RUN WHILE payfile HAS MORE 
-     // DATA TO PROCESS.
-	{
+	outPayfile << "Payrate      Hours    Gross Pay       Net Pay" << endl   << endl;      
+    
+	while(payfile >> hours){
 	   payfile >> payRate >> stateTax >> fedTax;
 	   gross = payRate * hours;
 	   net = gross - (gross * stateTax) - (gross * fedTax);
-	   cout << payRate << setw(15) << hours << setw(12) << gross
+	   outPayfile << payRate << setw(15) << hours << setw(12) << gross
               << setw(12)  << net << endl;
-	   payfile >> // FILL IN THE CODE TO FINISH THIS WITH THE APPROPRIATE  
-		          // VARIABLE TO BE INPUT
+
 	}
       payfile.close();
       return 0;
